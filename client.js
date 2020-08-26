9d514b0843c79c0ea6d3a05961fabf1ac6985df5
Vinay Baswa <vinaybaswa@gmail.com>
2020-08-26 19:40:06 +0000

    used conn.on to print a message on connection and named my snake as well

--- client.js
+++ client.js
@@ -2,10 +2,16 @@ const net = require('net');
 
 const connect = function() {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
+    host: '135.23.222.131',
+    port: 50542
   });
   conn.setEncoding('utf8');
+  conn.on('data', () => {
+    console.log('Successfully connected to game server');
+  });
+  conn.on('connect', () => {
+    conn.write('Name: VBZ')
+  });
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
